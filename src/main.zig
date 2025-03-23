2103a18de085b94ef35aa92e24d0f7e8d9d88cfa
Daniel1984 <aciddaniel@gmail.com>
2025-03-24 00:11:23 +0100

    ability to set server port by env var

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const httpz = @import("httpz");
 const zap = @import("zap");
 const App = @import("./app.zig");
+const Env = @import("./env.zig");
 const healtCheck = @import("./handlers/health-check.zig");
 const dummy = @import("./handlers/dummy.zig");
 
@@ -9,8 +10,11 @@ pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
 
+    var env = Env.init(allocator);
+    const port: u16 = env.getInt(u16, "PORT", 5888);
+
     var app = App.init();
-    var server = try httpz.Server(*App).init(allocator, .{ .port = 5882 }, &app);
+    var server = try httpz.Server(*App).init(allocator, .{ .port = port }, &app);
     var router = try server.router(.{});
 
     router.get("/dummy/:id", dummy.get, .{});
