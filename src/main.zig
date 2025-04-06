84b0bd539e8718f98f337a395e9f3aa7741c0dd8
Daniel1984 <aciddaniel@gmail.com>
2025-04-07 00:13:35 +0200

    some debugging in main.zig

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,8 @@ const App = @import("./app.zig");
 const Env = @import("./env.zig");
 const healtCheck = @import("./handlers/health-check.zig");
 const dummy = @import("./handlers/dummy.zig");
-const bfx = @import("./bfx/trading-pairs.zig");
+const bfx_trading_pairs = @import("./bfx/trading-pairs.zig");
+const bfx_candles = @import("./bfx/candles.zig");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -15,14 +16,18 @@ pub fn main() !void {
     var env = Env.init(allocator);
     const port: u16 = env.getInt(u16, "PORT", 5888);
 
-    var bfxTp = try bfx.init(allocator, 10);
-    try bfxTp.start();
+    var bfx_tp = try bfx_trading_pairs.init(allocator, 10);
+    try bfx_tp.start();
 
+    const bfx_pairs = try bfx_tp.getPairs();
     // debug
-    const bfx_pairs = try bfxTp.getPairs();
-    for (bfx_pairs) |pair| {
-        std.debug.print("bfx pair: {s}\n", .{pair});
-    }
+    // for (bfx_pairs) |pair| {
+    //     std.debug.print("bfx pair: {s}\n", .{pair});
+    // }
+
+    var bfxCndls = try bfx_candles.init(allocator, 2);
+    const candles = try bfxCndls.fetchCandles(bfx_pairs);
+    std.debug.print("bfx candles: {any}\n", .{candles[0]});
 
     var app = App.init();
     var server = try httpz.Server(*App).init(allocator, .{ .port = port, .address = "0.0.0.0" }, &app);
