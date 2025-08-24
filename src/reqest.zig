c5d5f0c47dab46d785486788c7b3151f993db619
Daniel1984 <aciddaniel@gmail.com>
2025-08-24 20:54:36 +0200

    maing request reusable package work with v0.15.1

--- src/reqest.zig
+++ src/reqest.zig
@@ -1,22 +1,18 @@
 const std = @import("std");
 const http = std.http;
 
-pub fn get(allocator: std.mem.Allocator, url: []const u8, max_response_size: usize) ![]u8 {
-    var client = http.Client{ .allocator = allocator };
+pub fn get(allocator: std.mem.Allocator, url: []const u8) ![]u8 {
+    var client: std.http.Client = .{
+        .allocator = allocator,
+    };
     defer client.deinit();
 
-    const uri = try std.Uri.parse(url);
-    var req = try client.request(.GET, uri, .{});
-    defer req.deinit();
+    var response: std.Io.Writer.Allocating = .init(allocator);
 
-    try req.sendBodiless();
-    var response = try req.receiveHead(&.{});
+    const result = try client.fetch(.{ .location = .{ .url = url }, .response_writer = &response.writer, .headers = .{ .accept_encoding = .{ .override = "identity" } } });
+    if (result.status != .ok) {
+        return error.HttpRequestFailed;
+    }
 
-    if (response.head.status != .ok) return error.HttpRequestFailed;
-
-    // Buffer for the reader
-    var reader_buffer: [4096]u8 = undefined;
-    var body_reader = response.reader(&reader_buffer);
-
-    return try body_reader.readAlloc(allocator, max_response_size);
+    return try response.toOwnedSlice();
 }
